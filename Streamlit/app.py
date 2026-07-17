e2b73462050aaa29cb341b737cd03fc818eeaa59
Ojasvi <bojas2005@gmail.com>
2026-07-17 10:11:44 +0530

    Final Commit

---
+++ Streamlit/app.py
@@ -0,0 +1,5 @@
+# run pip install streamlit in cmd
+#Activate virtual environment in terminal (venv) using this command .\.venv\Scripts\Activate.ps1
+import streamlit as st
+
+st.write("Hello World")
